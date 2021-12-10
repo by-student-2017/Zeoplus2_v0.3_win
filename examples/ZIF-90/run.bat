ca31e62fbf1197ab7bf4591c967ff7e631c3657b
by-student-2017 <studentsctest@gmail.com>
2021-12-10 10:19:47 +0900

    windows10

--- examples/ZIF-90/run.bat
+++ examples/ZIF-90/run.bat
@@ -1,6 +1,6 @@
 copy *.cif case.cif
-.\..\..\code\network.exe -ha -sa 1.2 1.2 2000 case.cif
-.\..\..\code\network -ha -vol 1.2 1.2 50000 case.cif
-.\..\..\code\network -ha -psd 1.2 1.2 50000 case.psd_histo case.cif
+.\..\..\code\network.exe -ha -sa 1.86 1.86 2000 case.cif
+.\..\..\code\network -ha -vol 1.86 1.86 50000 case.cif
+.\..\..\code\network -ha -psd 1.86 1.86 50000 case.psd_histo case.cif
 plot.gpl
-delet case.cif
+del case.cif
