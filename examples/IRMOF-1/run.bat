a4e7b33c026250cf1bb73c90c9193b8a69b7e3e5
by-student-2017 <studentsctest@gmail.com>
2021-12-13 11:58:51 +0900

    windows10

--- examples/IRMOF-1/run.bat
+++
@@ -1,6 +0,0 @@
-copy *.cif case.cif
-.\..\..\code\network.exe -ha -sa 1.2 1.2 2000 case.cif
-.\..\..\code\network -ha -vol 1.2 1.2 50000 case.cif
-.\..\..\code\network -ha -psd 1.2 1.2 50000 case.psd_histo case.cif
-plot.gpl
-delet case.cif
